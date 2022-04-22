92681c9278184fa567f797005e070a7b6da74179
brianmwevi <mwevibrian@gmail.com>
2022-04-22 21:06:54 +0300

    ft: user account -- add check_user method returning a boolean

--- pass_locker.py
+++ pass_locker.py
@@ -1,5 +1,9 @@
+import random as rand
+
 class User:
   users = []
+  user_count = 0
+
   def __init__(self, email, password):
     self.email = email
     self.password = password
@@ -7,18 +11,26 @@ class User:
     self.accounts = None
   
   def register(self):
-    if self in self.users:
+    user_exist = self.check_user()
+    if user_exist:
       return False
+    self.user_id = self.generate_user_id()
     self.users.append(self)
     return True
     
 
   def check_user(self):
-    # if
-    pass
+    if self in self.users:
+      return True
+    return False
+
+  @classmethod
+  def generate_user_id(cls):
+    cls.user_count+=1
+    return cls.user_count
 
   def login(self, username, password):
-    pass
+    pass 
 
   def update_account(self):
     pass
@@ -28,4 +40,5 @@ class User:
 
   def delete_account(self):
     pass
+
   
