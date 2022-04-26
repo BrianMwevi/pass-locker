922d9d60d0785fe072496fa117f432c6228b8cac
brianmwevi <mwevibrian@gmail.com>
2022-04-26 13:45:57 +0300

    patch: auto-generated credentials -- alert user for auto-generated credentials

--- credentials.py
+++ credentials.py
@@ -1,24 +1,61 @@
+import random as rand
+import string
 
 from pass_locker import User
+
+
 class Credentials(User):
 
-    def __init__(self, email, password, account_name, account_username, account_password=None):
+    def __init__(self, email, password):
         super().__init__(email, password)
-        self.account_name = account_name
-        self.account_username = account_username
-        self.account_password = account_password
 
-    def create_social_account(self):
-        pass
+    def create_social_account(self, account_name, username=None, password=None):
 
-    def generate_password(self):
-        pass
+        user, has_account = self.get_account(account_name)
+        if not has_account:
+            account = {}
+            account['account_name'] = account_name
+            account['username'] = username if username else self.generate_username()
+            account['password'] = password if password else self.generate_password()
+            user.accounts.append(account)
 
-    def check_credentials(self):
-        pass
+            return [account, True]
+        return [self, False]
 
-    def view_credentials(self):
-        pass
+    def generate_username(self, length=5):
+        characters = string.ascii_letters
+        username = "".join(rand.choice(characters) for _ in range(length))
+        return username
 
-    def delete_credentials(self):
-        pass
+    def generate_password(self, length=5):
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = "".join(rand.choice(characters) for _ in range(length))
+        return password
+
+    def get_account(self, account_name):
+        user = self.get_user()
+        for account in user.accounts:
+            if account['account_name'] == account_name:
+                return [user, account]
+        return [user, False]
+
+    def view_accounts(self, account_name=None):
+        user = self.get_user()
+        if account_name:
+            user, account = self.get_account(account_name)
+            return [account] if account else False
+        return user.accounts
+
+    def edit_account(self, account_name, username, password):
+        user = self.get_user()
+        user, account = self.get_account(account_name)
+        account['username'] = username
+        account['password'] = password
+        return account
+
+    def delete_account(self, account_name):
+        user, account = self.get_account(account_name)
+        if account:
+            user.accounts.remove(account)
+            return [user.accounts, True]
+        return [user.accounts, False]
